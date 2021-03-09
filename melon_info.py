584df67d383ea4c56ad9d0d6eae6897e87b7ff9b
qfuggett <qfuggett@gmail.com>
2021-03-09 03:19:57 +0000

    convert melons.py into nested dictionary, update print_melon function and for loop in melon_info.py

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,36 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melon_information
 
 
-def print_melon(name, seedless, price):
+def print_melon(name, price, seeded, flesh, weight, rind):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    # have_or_have_not = 'have'
+    # if seedless:
+    #     have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    seeded_or_not = True
+    if seeded:
+        seeded_or_not = False
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+    print(f"""{name}
+    seedless: {seeded_or_not}
+    price: ${price:.2f}
+    flesh_color: {flesh}
+    weight: {weight}
+    rind_color: {rind}
+    """)
+
+
+for i in melon_information:
+
+    print_melon(melon_information[1][i],
+    melon_information[2][i], 
+    melon_information[3][i], 
+    melon_information[4][i], 
+    melon_information[5][i], 
+    melon_information[6][i])
--- melons.py
+++ melons.py
@@ -1,23 +1,49 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
+melon_information = {
+    1: {
+        1: 'Honeydew',
+        2: 'Crenshaw',
+        3: 'Crane',
+        4: 'Casaba',
+        5: 'Cantaloupe',
+    },
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+    2: {
+        1: 0.99,
+        2: 2.00,
+        3: 2.50,
+        4: 2.50,
+        5: 0.99,
+    },
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+    3: {
+        1: True,
+        2: False,
+        3: False,
+        4: False,
+        5: False,
+    },
+
+    4: {
+        1: 'lime',
+        2: 'yellow',
+        3: 'orange',
+        4: 'white',
+        5: 'orange'
+    },
+
+    5: {
+        1: 'green',
+        2: 'dark green',
+        3: 'spotted',
+        4: 'lime',
+        5: 'dark green'
+    },
+
+    6: {
+        1: 2,
+        2: 1,
+        3: 3,
+        4: 2,
+        5: 2
+    }
 }
